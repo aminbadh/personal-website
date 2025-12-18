6a5b24c160a9f69e2a3884841e9be1f7859d3ff2
Amin BADH <aminbadh@gmail.com>
2025-12-18 17:51:06 +0100

    Update CV link to use internal redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: 'https://drive.google.com/file/d/1az1bGJ1LwQKUO5gKFHUP78rPd7IUmZ6s/view?usp=sharing',
+        permanent: false, // 307 redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
